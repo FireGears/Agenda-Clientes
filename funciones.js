fe03ba1296c9caf0c76f7ce86e5371e70f77a675
Alvaro Jose Ipanaque Hernandez <alvaroipanaquehernandez@gmail.com>
2024-03-20 22:54:45 -0500

    agenda fase 2

--- funciones.js
+++ funciones.js
@@ -7,3 +7,9 @@ function obtenerDatosHTML(nombre)
 {
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
+function obtenerContactos()
+{
+    let hoja = SpreadsheetApp.openById('11r8q0iT2lyDjSW7eBBx2DI4CA-_Y5GoGMPz8bm8ABno').getActiveSheet();
+    let datos = hoja.getDataRange().getValues();
+    return datos;
+}
